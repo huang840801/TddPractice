61924a68ee8260a0584c55386c93c7ada7a568da
GuanHongHuang <david840801@gmail.com>
2019-12-14 12:05:55 +0800

    Refactor: setup before

--- app/src/test/java/com/guanhong/tddpractice/MathTest.kt
+++ app/src/test/java/com/guanhong/tddpractice/MathTest.kt
@@ -3,13 +3,22 @@ package com.guanhong.tddpractice
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 class MathTest {
+
+    lateinit var math: Math
+
+    @Before
+    fun setup(){
+
+        math = Math()
+    }
 
     @Test
     fun addTwoNum() {
 
-        val expected = Math().addTwoNum(1, 3)
+        val expected = math.addTwoNum(1, 3)
         val actual = 4
 
         assertEquals(expected, actual)
@@ -18,7 +27,7 @@ class MathTest {
     @Test
     fun testNum1LessThanNum2() {
 
-        val actual = Math().getMin(3, 7)
+        val actual = math.getMin(3, 7)
         val expect = 3
 
         assertEquals(expect, actual)
@@ -27,7 +36,7 @@ class MathTest {
     @Test
     fun testNum2LessThanNum1() {
 
-        val actual = Math().getMin(8, 7)
+        val actual = math.getMin(8, 7)
         val expect = 7
 
         assertEquals(expect, actual)

e6fe1993afbf687d2ea4b5f31ef9ca4255ac2b07
GuanHongHuang <david840801@gmail.com>
2020-01-01 11:23:14 +0800

    Add Toast test

--- app/src/androidTest/java/com/guanhong/tddpractice/RegisterTest.kt
+++ app/src/androidTest/java/com/guanhong/tddpractice/RegisterTest.kt
@@ -1,14 +1,15 @@
 package com.guanhong.tddpractice
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,13 +21,21 @@ class RegisterTest {
     val activityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun register_success_should_start_RegisterSuccessActivity(){
+    fun register_success_should_start_RegisterSuccessActivity() {
 
         onView(withId(R.id.account)).perform(typeText("q1111111"), ViewActions.closeSoftKeyboard())
-        onView(withId(R.id.password)).perform(typeText("w22222222"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.password)).perform(
+            typeText("w22222222"),
+            ViewActions.closeSoftKeyboard()
+        )
         onView(withId(R.id.registerButton)).perform(click())
 
         onView(withId(R.id.successText)).check(matches(withText("Register Success")))
         onView(withId(R.id.successText)).check(matches(isDisplayed()))
+
+        onView(withText("註冊成功")).inRoot(withDecorView(not(activityTestRule.activity.window.decorView)))
+            .check(
+                matches(isDisplayed())
+            )
     }
 }

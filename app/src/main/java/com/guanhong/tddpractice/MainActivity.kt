77c49ba40bbbc3df5f55312c453571bbc827d691
GuanHongHuang <david840801@gmail.com>
2020-01-13 21:55:20 +0800

    Delete LeetCode and test

--- app/src/main/java/com/guanhong/tddpractice/MainActivity.kt
+++ app/src/main/java/com/guanhong/tddpractice/MainActivity.kt
@@ -3,11 +3,9 @@ package com.guanhong.tddpractice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.guanhong.tddpractice.product.ProductActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import tddpracticesample.Repository
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val test = LeetCode().mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", arrayOf("hit"))
-
-        Log.d("Huang"," aaa "+ test)
 
         sharedPreferenceManager = SharedPreferenceManager(this)
 
@@ -42,6 +36,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-//        startActivity(Intent(this, ProductActivity::class.java))
+        startActivity(Intent(this, ProductActivity::class.java))
     }
 }
